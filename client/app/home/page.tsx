0cfdecd0c93f0ece7886074ca63b01716d663995
Rodi Gemici <rodix08@gmail.com>
2026-03-10 13:45:43 +0000

    Added initial page + installed MUI

---
+++ client/app/home/page.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@mui/material";
+import Image from "next/image";
+
+export default function HomePage() {
+  return (
+    <Box sx={{display:"flex", justifyContent:"center", alignItems:"center", height:"100vh"}}>Example Page</Box>
+
+  );
+}
